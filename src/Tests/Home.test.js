975e51bb225daac086d3efa88b2bcdb9e782e70c
dwright1989 <debbiewright@live.co.uk>
2022-11-07 14:23:11 +0000

    Home page testing

--- src/Tests/Home.test.js
+++ src/Tests/Home.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../Components/Home';
 
-test('renders Home', () => {
-  render(<Home />);
-  const homeContent = screen.getByText(/i am the home/i);
-  expect(homeContent).toBeInTheDocument();
-});
+
+
+describe('home page testing', ()=>
+{
+      test('renders heading', ()=>{
+        render(<Home />);
+        const brandHeading = screen.getByRole('heading');
+        expect(brandHeading).toBeInTheDocument();
+      });
+
+      test('renders paragraph', ()=>{
+        render(<Home />);
+        const para = screen.getByText(/a fake portfolio store/i);
+        expect(para).toBeInTheDocument();
+      });
+
+      test('renders button', ()=>{
+        render(<Home />);
+        const button = screen.getByRole('button', {name: /shop now/i});
+        expect(button).toBeInTheDocument();
+      });
+
+}
+)
+
