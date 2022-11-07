f6d2ff026e1e48966a9b700b736c00822873b6ac
dwright1989 <debbiewright@live.co.uk>
2022-11-07 14:18:21 +0000

    Nav bar test updated

--- src/Tests/NavBar.test.js
+++ src/Tests/NavBar.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import NavBar from '../Components/NavBar';
 
-test('renders nav bar', () => {
-  render(<NavBar />);
-  const navContent = screen.getByText(/i am the nav bar/i);
-  expect(navContent).toBeInTheDocument();
-});
+
+describe('nav bar testing', ()=>
+{
+      test('renders heading', ()=>{
+        render(<NavBar />);
+        const brandHeading = screen.getByRole('heading');
+        expect(brandHeading).toBeInTheDocument();
+      });
+
+      test('renders links', ()=>{
+        render(<NavBar />);
+        const link = screen.getByRole('link', {name: /home/i});
+        expect(link).toBeInTheDocument();
+        const link2 = screen.getByRole('link', {name: /products/i});
+        expect(link2).toBeInTheDocument();
+        const link3 = screen.getByRole('link', {name: /about/i});
+        expect(link3).toBeInTheDocument();
+        const link4 = screen.getByRole('link', {name: /cart/i});
+        expect(link4).toBeInTheDocument();
+      });
+}
+)
+
