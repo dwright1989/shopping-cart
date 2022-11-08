6563a03353a17fd814cdd1861e934dbb67c90e2d
dwright1989 <debbiewright@live.co.uk>
2022-11-08 11:49:28 +0000

    Shop ready to import cards for products

--- src/Components/Products/ProductItems.js
+++ src/Components/Products/ProductItems.js
@@ -1,61 +1,61 @@
 const products = [
         {
             id: 0, 
-            url: require("../Images/blackAndWhiteLongSleeved.png"), 
+            url: require("../../Images/blackAndWhiteLongSleeved.png"), 
             name: "Black and White Long Sleeved Top", 
             price: 15.99
         },
         {
             id: 1, 
-            url: require("../Images/blackZipper.png"), 
+            url: require("../../Images/blackZipper.png"), 
             name: "Black Zipper", 
             price: 25.99
         },
         {
             id: 2, 
-            url: require("../Images/blueSliders.png"), 
+            url: require("../../Images/blueSliders.png"), 
             name: "Sliders", 
             price: 7.99
         },
         {
             id: 3, 
-            url: require("../Images/greyJoggers.png"), 
+            url: require("../../Images/greyJoggers.png"), 
             name: "Grey Joggers", 
             price: 18.99
         },
         {
             id: 4, 
-            url: require("../Images/greyShoes.png"), 
+            url: require("../../Images/greyShoes.png"), 
             name: "Shoes", 
             price: 45.99
         },
         {
             id: 5, 
-            url: require("../Images/greyTShirt.png"), 
+            url: require("../../Images/greyTShirt.png"), 
             name: "Grey T-Shirt", 
             price: 10.99
         },
         {
             id: 6, 
-            url: require("../Images/tieDieTShirt.png"), 
+            url: require("../../Images/tieDieTShirt.png"), 
             name: "Tie Die T-Shirt", 
             price: 11.99
         },
         {
             id: 7, 
-            url: require("../Images/whiteHoodie.png"), 
+            url: require("../../Images/whiteHoodie.png"), 
             name: "white Hoodie", 
             price: 27.99
         },
         {
             id: 8, 
-            url: require("../Images/whiteShoes.png"), 
+            url: require("../../Images/whiteShoes.png"), 
             name: "White Trainers", 
             price: 40.99
         },
         {
             id: 9, 
-            url: require("../Images/whiteSliders.png"), 
+            url: require("../../Images/whiteSliders.png"), 
             name: "White Sliders", 
             price: 7.99
         },
@@ -63,3 +63,5 @@ const products = [
 
     
 ];
+
+export default products;
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,7 +1,14 @@
+import Products from "../Components/Products/ProductItems";
+
 export default function Shop(){
     return (
         <div id="shop" className="page">
-            I am the shop
+            <div id="products">
+                {console.log(JSON.stringify(Products))}
+                {Products.map( (product) =>{
+                    return <p>This is product {product.id}</p>
+                })}
+            </div>
         </div>
     );
 }
