efd07d5c5b72cdfdc15aaac1b531a24c10690e77
dwright1989 <debbiewright@live.co.uk>
2022-11-09 11:15:15 +0000

    Card test added

---
+++ src/Tests/Card.test.js
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import Card from '../Components/Card';
+
+describe('card testing renders', ()=>
+{
+
+    const item = {
+        id: 0, 
+        url: "../../Images/blackAndWhiteLongSleeved.png", 
+        name: "Long Sleeved Top", 
+        price: 15.99
+    };
+
+    test('renders image', () => {
+        const onChangeMock = jest.fn();
+        render(<Card product={item} handleAddToBasket={onChangeMock}/>);
+        const image = screen.getByRole("img");
+        expect(image).toBeInTheDocument();
+    });
+
+    test('renders name', () => {
+        const onChangeMock = jest.fn();
+        render(<Card product={item} handleAddToBasket={onChangeMock}/>);
+        const namePara = screen.getByText(item.name);
+        expect(namePara).toBeInTheDocument();
+    });
+
+    test('renders price', () => {
+        const onChangeMock = jest.fn();
+        render(<Card product={item} handleAddToBasket={onChangeMock}/>);
+        const pricePara = screen.getByText(/15.99/i);
+        expect(pricePara).toBeInTheDocument();
+    });
+
+    test('renders add to basket button', () => {
+        const onChangeMock = jest.fn();
+        render(<Card product={item} handleAddToBasket={onChangeMock}/>);
+        const addToBasketButton = screen.getByRole('button', {name: /Add To Basket/i});
+        expect(addToBasketButton).toBeInTheDocument();
+    });
+
+});
