d58b909619331eb62a23dcfc4ccf542960ded7f2
dwright1989 <debbiewright@live.co.uk>
2022-11-11 14:17:07 +0000

    Div created to show products in cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,9 @@
 export default function Cart(props){
     return (
         <div id="cart" className="page">
-            <p>{JSON.stringify(props)}</p>
+            <div id="cartContainer">
+                <p>{JSON.stringify(props)}</p>
+            </div>
         </div>
     );
 }
