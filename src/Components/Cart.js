f766ca3b86fd579f102496e3dc1ba598f8fe1520
dwright1989 <debbiewright@live.co.uk>
2022-11-11 15:10:22 +0000

    Able to increase the quantity at cart level

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,12 +3,13 @@ import Products from "./Products/ProductItems";
 
 export default function Cart(props){
     console.log(JSON.stringify(props));
-    const {basket, totals} = props;
+    const {basket, totals, handleAddToBasket} = props;
     let items = basket.products;
     function getProductFromId(theId){
         let productMatch = Products.find(({id})=> id===theId);
         return productMatch;
     }
+
     return (
         <div id="cart" className="page">
             <div id="cartContainer">
@@ -20,6 +21,11 @@ export default function Cart(props){
                                         <p>{itemToDisplay.name}</p>
                                         <img src={itemToDisplay.url} alt="product"/>
                                         <p>£{itemToDisplay.price}</p>
+
+                                        <div className="quantityEditor">
+                                                <button onClick={handleAddToBasket.bind(this, item.id)}>+</button>
+                                                <p>{item.quantity}</p>
+                                        </div>
                             </div>)
                     })}
                 </div>
