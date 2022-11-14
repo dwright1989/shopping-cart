e53ce7c1b63968aef9cbc8fce42d1f3eaa3d9837
dwright1989 <debbiewright@live.co.uk>
2022-11-14 09:52:00 +0000

    Text added when cart is empty

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,40 +1,58 @@
-
 import Products from "./Products/ProductItems";
 
-export default function Cart(props){
-    console.log(JSON.stringify(props));
-    const {basket, totals, handleAddToBasket, handleRemoveFromBasket} = props;
-    let items = basket.products;
-    function getProductFromId(theId){
-        let productMatch = Products.find(({id})=> id===theId);
-        return productMatch;
-    }
+export default function Cart(props) {
+  console.log(JSON.stringify(props));
+  const { basket, totals, handleAddToBasket, handleRemoveFromBasket } = props;
+  let items = basket.products;
+  function getProductFromId(theId) {
+    let productMatch = Products.find(({ id }) => id === theId);
+    return productMatch;
+  }
 
-    return (
-        <div id="cart" className="page">
-            <div id="cartContainer">
-                <h1>Cart</h1>
-                <div id="cartItems">
-                    {items.map(item=>{
-                        let itemToDisplay = getProductFromId(item.id);
-                        return (<div className="cart-item">
-                                        <p>{itemToDisplay.name}</p>
-                                        <img src={itemToDisplay.url} alt="product"/>
-                                        <p>£{itemToDisplay.price}</p>
+  return (
+    <div id="cart" className="page">
+      <div id="cartContainer">
+        <h1>Cart</h1>
+        {items.length > 0 && (
+          <div>
+            <div id="cartItems">
+              {items.map((item) => {
+                let itemToDisplay = getProductFromId(item.id);
+                return (
+                  <div className="cart-item">
+                    <p>{itemToDisplay.name}</p>
+                    <img src={itemToDisplay.url} alt="product" />
+                    <p>£{itemToDisplay.price}</p>
 
-                                        <div className="quantity-editor">
-                                                <button onClick={handleAddToBasket.bind(this, item.id)}>+</button>
-                                                <p>{item.quantity}</p>
-                                                <button onClick={handleRemoveFromBasket.bind(this, item.id)}>-</button>
-                                        </div>
-                                        <hr/>
-                            </div>)
-                    })}
-                </div>
-                <p>Total: £{totals.price.toFixed(2)}</p>
-                <button>Checkout</button>
+                    <div className="quantity-editor">
+                      <button onClick={handleAddToBasket.bind(this, item.id)}>
+                        +
+                      </button>
+                      <p>{item.quantity}</p>
+                      <button
+                        onClick={handleRemoveFromBasket.bind(this, item.id)}
+                      >
+                        -
+                      </button>
+                    </div>
+                    <hr />
+                  </div>
+                );
+              })}
             </div>
-        </div>
-    );
+            <p>Total: £{totals.price.toFixed(2)}</p>
+            <button>Checkout</button>
+          </div>
+        )}
+        {items.length === 0 && (
+          <div>
+            <p>No items currently in your cart.</p>
+            <p>
+              <a href="/Shop">Click here</a> to shop.
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
-
