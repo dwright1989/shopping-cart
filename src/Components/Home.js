e0c3447cf61d01a553385aedc772369d1c5d401d
dwright1989 <debbiewright@live.co.uk>
2022-11-15 11:09:42 +0000

    Shop now link

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ export default function Home(){
         <div id="home" className="page">
             <h1 className="brand">Clothes<span className="tertiary">4</span>You</h1>
             <p>A fake portfolio store</p>
-            <form action="/Shop">
+            <form action="../Shop">
                 <input id="shopButton" type="submit" value="Shop Now"/>
             </form>
         </div>
