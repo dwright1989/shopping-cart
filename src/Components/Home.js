7988ef16692d2da3c3ebc8f7b9bcf11f72413e38
dwright1989 <debbiewright@live.co.uk>
2022-11-15 13:09:30 +0000

    Changes to links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,12 @@
+import {Link} from "react-router-dom";
+
+
 export default function Home(){
     return (
         <div id="home" className="page">
             <h1 className="brand">Clothes<span className="tertiary">4</span>You</h1>
             <p>A fake portfolio store</p>
-            <form action="../Shop">
-                <input id="shopButton" type="submit" value="Shop Now"/>
-            </form>
+            <Link to="../shop" id="shopButton">Shop Now</Link>
         </div>
     );
 }
